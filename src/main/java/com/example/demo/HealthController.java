96edc1a48f99f2ac966c702f1316001ed398a502
Chintana Meegamarachchi <chintana@appsolve.com.au>
2017-08-20 11:57:45 +0800

    Builds and runs REST api

---
+++ src/main/java/com/example/demo/HealthController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HealthController {
+
+    @GetMapping(value="/hchk")
+    public String healthCheck()
+    {
+        return "Ping from api!";
+    }
+}
