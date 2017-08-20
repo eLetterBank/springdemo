44a5689b54c98a93b781244a69fd118adcb695c3
Chintana Meegamarachchi <chintana@appsolve.com.au>
2017-08-20 15:16:01 +0800

    Enables logger - log4j

--- src/main/java/com/example/demo/HealthController.java
+++ src/main/java/com/example/demo/HealthController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class HealthController {
 
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
     @GetMapping(value="/hchk")
     public String healthCheck()
     {
+        logger.info(this.getClass().getSimpleName());
         return "Ping from api!";
     }
 }
