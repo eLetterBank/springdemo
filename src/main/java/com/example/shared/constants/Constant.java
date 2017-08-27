9798b31afc640aea01af5d0388864db7eaa31760
Mirage <eletterbank@gmail.com>
2017-08-28 00:41:24 +1000

    Adds AuthenticationFilter

---
+++ src/main/java/com/example/shared/constants/Constant.java
@@ -0,0 +1,12 @@
+package com.example.shared.constants;
+
+public class Constant {
+    Constant() {
+        throw new IllegalStateException("Constant class");
+    }
+
+    //HTTP header values
+    public static final String X_VSOLV_KEY = "x-vsolv-key";
+    public static final String CONTENT_TYPE = "application/json;charset=UTF-8";
+
+}
